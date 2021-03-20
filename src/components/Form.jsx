524b8dbd61f3cd4e7f149dc65928d4b33488ebfc
sarabluekens <sara.bluekens@student.howest.be>
2021-03-20 20:45:33 +0100

    form initialized

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,6 +4,11 @@ const Form = () => {
     return (
         <section className = "form">
             <p>form</p>
+            <form  onSubmit={handleSubmitPost}>
+                <input  type="text" id="description" name="description"
+                    placeholder="Write a description" value={description}  />
+                <button>post</button>
+            </form>
         </section>
     );
 };
