9ed83d6d8c7524c73fee87d710421db69e5f4bb2
sarabluekens <sara.bluekens@skynet.be>
2021-09-08 15:41:10 +0200

    publish assignment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import styles from "./Home.module.css";
 const Home = () => {
     return ( 
         <section className={styles.home}>
-            <h1 className={styles.homeTitle}>To The GRAAAAAVE</h1>
+            <h1 className={styles.homeTitle}>To The GRAAAAAAVE</h1>
             <p className={styles.homeText}>Welcome to the crazy calculator for Death</p>
         </section>
      );
